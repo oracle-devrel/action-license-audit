0ac30908cd8908611b5300710d29beea218e6dba
Brett Terpstra <me@brettterpstra.com>
2023-05-17 13:31:39 -0500

    I don

--- license_check.rb
+++ license_check.rb
@@ -3,8 +3,10 @@
 # - Removes deprecated ::set-output
 
 require 'json'
+require 'shellwords'
 
 file_name = 'licenses.json'
+output = []
 
 if ARGV.length > 0
   file_name = ARGV[0]
@@ -26,7 +28,7 @@ end
 
 if file_data.length <= 0
   puts "WARNING - no data read (0 byte JSON file)."
-  ENV['GITHUB_OUTPUT'] << "name=unapproved_licenses::false\n"
+  output << "name=unapproved_licenses::false"
   exit(0)
 end
 
@@ -40,7 +42,7 @@ json_data['files'].each do |f|
       if !unapproved_licenses.include?(l['key'])
         unapproved_licenses[l['key']] = []
       end
-      
+
       if !unapproved_licenses[l['key']].include?(f['path'])
         unapproved_licenses[l['key']] << f['path']
       end
@@ -50,8 +52,10 @@ end
 
 if unapproved_licenses.count > 0
   puts "ERROR - found some licenses that require further inspection:\n#{unapproved_licenses}"
-  ENV['GITHUB_OUTPUT'] << "name=unapproved_licenses::true\n"
+  output << "name=unapproved_licenses::true"
 else
   puts "All licenses found were approved for use."
-  ENV['GITHUB_OUTPUT'] << "name=unapproved_licenses::false\n"
+  output << "name=unapproved_licenses::false"
 end
+
+`echo #{Shellwords.escape(output.join("\n"))} >> GITHUB_OUTPUT`
